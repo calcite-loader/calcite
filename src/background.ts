1be443372da8bb454887007b5b33f378d9c6468b
Grady Link <github@grady.link>
2026-05-01 11:33:47 -0700

    fix(devtools): background error

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,9 @@
 const api = typeof browser !== "undefined" ? browser : chrome;
 
 api.runtime.onMessage.addListener((message, sender) => {
-  try {
-    (api.runtime.sendMessage as (message: any) => void)({
-      type: "DEVTOOLS",
-      payload: message,
-      tabId: sender.tab?.id,
-    });
-  } catch (e) {}
+  (api.runtime.sendMessage as (message: any) => Promise<void>)({
+    type: "DEVTOOLS",
+    payload: message,
+    tabId: sender.tab?.id,
+  }).catch(() => {});
 });
